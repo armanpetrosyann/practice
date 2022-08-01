b3138183463e0265a27d96afd4d4bc9d78da2a81
arman.p <arman.p@F4RPDEV-62>
2022-08-01 11:07:53 +0400

    Added program. Print X graphic in console.

---
+++ Exercises/PrintX.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    internal class PrintX
+    {
+        static void PrintInConsoleX()
+        {
+            int i;
+            int j;
+            char symOne = '*';
+            char symTwo = '#';
+
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    if (i == j || (i + j == 10))
+                    {
+                        Console.Write(symOne);
+                    }
+                    Console.Write(symTwo);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
